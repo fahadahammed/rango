cfcfe1ba0c6da97c2dc4bef87bb6151e39204b8f
fahadahammed <iamfahadahammed@gmail.com>
2019-10-19 07:19:18 +0000

    Initiated Project Rango

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+
+import socket
+
+import datetime
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def api_home():
+
+    hostname = socket.gethostname()
+
+    ip = socket.gethostbyname(hostname)
+
+    return jsonify({
+
+            "hostname": hostname,
+
+            "ip": ip,
+
+            "timestamp": str(datetime.datetime.now()),
+
+            "application": "Rango"
+
+        })
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=9355)
